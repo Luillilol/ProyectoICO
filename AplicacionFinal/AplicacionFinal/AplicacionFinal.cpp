028a3a58e7f513824d1377355451b18c38a1e1c5
Falcón <luis_adrian359@comunidad.unam.mx>
2022-11-30 20:02:38 -0600

    FEAT: Creación de función

--- AplicacionFinal/AplicacionFinal/AplicacionFinal.cpp
+++ AplicacionFinal/AplicacionFinal/AplicacionFinal.cpp
@@ -4,6 +4,21 @@
 
 
 #include <stdio.h>
+void gradoPolinomio(char *polinomio){
+	int i = 0;
+	char grado[2];
+
+	for (i = 0; i < 100; i++) {
+		if (polinomio[i] == '^' && polinomio != '\0')
+		{
+			
+		}
+
+	}
+
+}
+
+
 int main(void) {
 
 	char polinomio[100];
@@ -11,9 +26,17 @@ int main(void) {
 
 	printf("Introduce el polinomio:");
 	gets_s(polinomio);
-
 	printf("El polinomio escrito fue: %s", polinomio);
+
+
+	//Checar el grado del polinomio más grande
+	gradoPolinomio(polinomio);
+
+
+
 	
+	
+
 	
 	return 0;
 }
