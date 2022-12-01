a782e8f826faa3e697105c065896ee018d0e3dc6
Falcón <luis_adrian359@comunidad.unam.mx>
2022-11-30 20:25:22 -0600

    Creación rama terminos

--- AplicacionFinal/AplicacionFinal/AplicacionFinal.cpp
+++ AplicacionFinal/AplicacionFinal/AplicacionFinal.cpp
@@ -4,18 +4,23 @@
 
 
 #include <stdio.h>
+void contadorTerminos( char *polinomio) {
+
+}
+
 void gradoPolinomio(char *polinomio){
-	int i = 0;
-	char grado[2];
+	int i = 0, j=0;
+	char grado[10];
 
 	for (i = 0; i < 100; i++) {
-		if (polinomio[i] == '^' && polinomio != '\0')
+		if (polinomio[i] == '^' && polinomio[i] != '\0')
 		{
-			
+			grado[j] = polinomio[i + 1];
+			j++;
 		}
 
 	}
-
+	printf("Grados: %s", grado);
 }
 
 
@@ -28,7 +33,8 @@ int main(void) {
 	gets_s(polinomio);
 	printf("El polinomio escrito fue: %s", polinomio);
 
-
+	//cuenta los términos
+	contadorTerminos(polinomio);
 	//Checar el grado del polinomio más grande
 	gradoPolinomio(polinomio);
 
